8b94dbf69383b3375c1367a051f7326de6702163
cap-ma <uone2323@gmail.com>
2023-07-06 14:19:02 +0500

    static files

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -126,3 +127,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_FILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "website" / "staticfiles"
